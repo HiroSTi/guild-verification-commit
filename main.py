bf29841a437cbfc9f006b8b2a9162455be9e567b
Hiro <89306365+HiroSTi@users.noreply.github.com>
2026-03-05 23:29:34 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:29:26.815Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T20:29:34.313Z
+Commit: Introduce real-time notifications
