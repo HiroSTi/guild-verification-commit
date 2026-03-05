6ddad8776ada8a88f9573222c2436f94974fe1f9
Hiro <89306365+HiroSTi@users.noreply.github.com>
2026-03-05 23:29:33 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:29:29.964Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T20:29:33.437Z
+Commit: Patch security vulnerability in authentication
